35158eb6b7908b00d28f24571c991f3bd37c5c7c
chimaskyy <chiamakahj@gmail.com>
2023-11-06 22:28:25 +0100

    base class created

---
+++ models/base_model.py
@@ -0,0 +1,34 @@
+#!/usr/bin/python3
+"""This model contains the base class
+"""
+import uuid 
+from datetime import datetime
+
+
+class BaseModel:
+    """
+    base class
+    """
+    def __init__(self):
+        self.id = str(uuid.uuid4())
+        self.created_at = datetime.now()
+        self.updated_at = datetime.now()
+
+    def save(self):
+        updated_at = datetime.now()
+
+    def to_dict(self):
+        diction = {}
+        diction["__class__"] = self.__class__.__name__
+        
+        for key, value in self.__dict__.items():
+            if isinstance(value, datetime):
+                diction[key] = value.isoformat()
+            else:
+                diction[key] = value
+        return diction
+
+    def __str__(self):
+        return "[{}] ({}) {}".format(self.__class__.__name__, self.id, self.__dict__)
+
+
